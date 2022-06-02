9e21f3cf59500b105405df944921223ab7012ada
Andriy <81190009+kulakovskyi@users.noreply.github.com>
2022-06-02 14:25:58 +0300

    Delete source directory

--- source/src/assets/js/zamena.js
+++
@@ -1,7 +0,0 @@
-const btnCall = document.querySelector(".link__on__tel");
-
-if (window.innerWidth <= 575) {
-  btnCall.innerHTML = "Записаться";
-} else {
-  btnCall.innerHTML = "Обратный звонок";
-}
