acc97ef3fa7fc9c8809874711f12392100b9e4f7
Andriy <81190009+kulakovskyi@users.noreply.github.com>
2022-06-03 16:08:54 +0300

    Add files via upload

--- src/assets/js/filter.js
+++ src/assets/js/filter.js
@@ -2,6 +2,8 @@ const servicesItems = document.querySelectorAll(".services__item");
 
 const buttonsContainer = document.querySelector(".services__buttons");
 
+const buttonAll = document.querySelectorAll(".services__button");
+
 /****************filter adaptive*************/
 
 const buttonCategory = document.querySelector(".category");
@@ -15,6 +17,10 @@ buttonCategory.addEventListener("click", () => {
 buttonsContainer.addEventListener("click", (event) => {
   //проверяем кли только по кнопкам
   if (event.target.tagName !== "BUTTON") return false;
+  buttonAll.forEach((item) => {
+    item.classList.remove("_active-btn");
+  });
+  event.target.classList.add("_active-btn");
   buttonsContainer.classList.remove("_active-category");
   buttonCategory.innerHTML = event.target.value;
   //получаем дата атрибут с кнопки
